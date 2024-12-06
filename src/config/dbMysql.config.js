8f7ba1e747d12cb573a4ad65d554b231196dcf02
coco <constanzacatalano@gmail.com>
2024-12-06 15:16:37 -0300

    bug fix

--- src/config/dbMysql.config.js
+++ src/config/dbMysql.config.js
@@ -12,11 +12,15 @@ const pool = mysql.createPool(
     }
 )
 
-pool.getConnection().then((result) => {
-    console.log('Conexión con MySql exitosa')
-})
-.catch((err) => {
-    console.error('Error en la conexión: ', err)
-})
+pool.getConnection()
+    .then(async (connection) => {
+        
+        await connection.query(`USE ${ENVIROMENT.MYSQL.DATABASE}`)
+        console.log('Conexión con MySQL exitosa y base de datos seleccionada')
+        connection.release()
+    })
+    .catch((err) => {
+        console.error('Error en la conexión: ', err)
+    })
 
 export default pool
